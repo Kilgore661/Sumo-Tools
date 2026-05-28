5a2e48fda7c2554985ef9b1b8671ea9f7129b364
Kilgore661 <kilgore661@gmail.com>
2026-05-28 09:43:05 +0100

    Out of sequence tinkering with landing page.

--- src/products/make_site2/runtime/site-refactor/core/context.js
+++ src/products/make_site2/runtime/site-refactor/core/context.js
@@ -11,7 +11,7 @@ function siteContext() {
     return { id: "remote", label: "Remote Site", titlePrefix: "REMOTE" };
   }
   if (hostname.startsWith("192.168.")) {
-    return { id: "local", label: "Local Site" };
+    return { id: "local", label: "Danger Dr. Smith! Waterfall model detected! - B-9" };
   }
   return { id: "preview", label: "Preview Site", titlePrefix: "PREVIEW" };
 }
