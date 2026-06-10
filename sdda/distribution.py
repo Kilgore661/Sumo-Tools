40fb85ab2eb42e1754587ac508de42a7d76e5dcd
Ben Thompson <kilgore661@gmail.com>
2026-06-10 09:35:17 +0100

    Recognise mode dependent deployment source distribution

--- sdda/distribution.py
+++ sdda/distribution.py
@@ -3,7 +3,13 @@ from __future__ import annotations
 from .models import DistributionCandidateRecord, FileFamilyClassificationRecord
 
 INCLUDE_CLASSIFICATIONS = {"required_distribution_input", "environment_setting"}
-REVIEW_CLASSIFICATIONS = {"possible_efficiency_cache", "possible_pipeline_intermediate", "possible_state_or_control_file", "unknown_review_needed"}
+REVIEW_CLASSIFICATIONS = {
+    "mode_dependent_deployment_source",
+    "possible_efficiency_cache",
+    "possible_pipeline_intermediate",
+    "possible_state_or_control_file",
+    "unknown_review_needed",
+}
 EXCLUDE_CLASSIFICATIONS = {"generated_output", "generated_then_consumed", "internet_source"}
 
 
