ae9b0bbd162bcd8801faa262bb1e56a5156acb52
Ben Thompson <kilgore661@gmail.com>
2026-06-17 12:20:26 +0100

    runtime: implement generic chart layout policy module

--- src/products/make_site2/runtime/site-refactor/ui/charts/generic-layout-policy.js
+++ src/products/make_site2/runtime/site-refactor/ui/charts/generic-layout-policy.js
@@ -1,1 +1,31 @@
-export { stackedBarLayout, groupedChartLayout, orderedBarLayout, categoryBarLayout } from "./generic-layouts.js";
+import { genericXAxisTickAngle } from "./generic-angle-policy.js";
+import {
+  categoryBarLayout,
+  groupedChartLayout as baseGroupedChartLayout,
+  orderedBarLayout as baseOrderedBarLayout,
+  stackedBarLayout as baseStackedBarLayout,
+} from "./generic-layouts.js";
+
+function setXAxisTickAngle(artifact, layout) {
+  layout.xaxis.tickangle = genericXAxisTickAngle(artifact);
+  return layout;
+}
+
+function stackedBarLayout(artifact, rowsBySource) {
+  return setXAxisTickAngle(artifact, baseStackedBarLayout(artifact, rowsBySource));
+}
+
+function groupedChartLayout(artifact, rowsBySource) {
+  return setXAxisTickAngle(artifact, baseGroupedChartLayout(artifact, rowsBySource));
+}
+
+function orderedBarLayout(artifact, trace) {
+  return setXAxisTickAngle(artifact, baseOrderedBarLayout(artifact, trace));
+}
+
+export {
+  stackedBarLayout,
+  groupedChartLayout,
+  orderedBarLayout,
+  categoryBarLayout,
+};
