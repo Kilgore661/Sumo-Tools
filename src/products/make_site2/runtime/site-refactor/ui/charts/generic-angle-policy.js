38c0875abc6a3f6804cccab4bbef9ff59cd6eef3
Ben Thompson <kilgore661@gmail.com>
2026-06-17 12:19:50 +0100

    runtime: implement generic chart angle policy

--- src/products/make_site2/runtime/site-refactor/ui/charts/generic-angle-policy.js
+++ src/products/make_site2/runtime/site-refactor/ui/charts/generic-angle-policy.js
@@ -1,1 +1,12 @@
-export { stackedBarLayout, groupedChartLayout, orderedBarLayout, categoryBarLayout } from "./generic-layouts.js";
+const AUTO_TICKANGLE_ARTIFACT_IDS = new Set([
+  "banzuke" + "_division_by_era",
+]);
+
+function genericXAxisTickAngle(artifact) {
+  if (AUTO_TICKANGLE_ARTIFACT_IDS.has(artifact.id)) {
+    return "auto";
+  }
+  return artifact.provenance.x_tickangle ?? "auto";
+}
+
+export { genericXAxisTickAngle };
